ea3f45001f70d3f7b62e99a9f68665df5f21c798
ellenchoi97 <ellenchoi97@gmail.com>
2019-03-16 20:50:13 -0700

    added script for playing keyboard sounds

--- Assets/Synthesizer.cs
+++ Assets/Synthesizer.cs
@@ -8,6 +8,11 @@ public class Synthesizer : MonoBehaviour
     public Transform black_key_prefab;
     public Transform type_button_prefab;
 
+    float[] white_key_freq = { 32.7f, 36.71f, 41.2f, 43.65f, 49.0f, 55.0f, 61.74f };
+    float[] black_key_freq = { 34.65f, 38.89f, 46.25f, 51.91f, 58.27f };
+    int whiteIndex = 0;
+    int blackIndex = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +24,18 @@ public class Synthesizer : MonoBehaviour
         {
             newKey = Instantiate(white_key_prefab, synthesizer.transform);
             newKey.localPosition = new Vector3(x, 0.17f, -0.15f);
+            SynthSound thisScript = newKey.gameObject.GetComponent<SynthSound>();
+            thisScript.base_frequency = white_key_freq[whiteIndex];
+            thisScript.calcOctaveFreq((i / 7) + 2);
             x += 0.327f;
+            if(whiteIndex == 6)
+            {
+                whiteIndex = 0;
+            }
+            else
+            {
+                whiteIndex++;
+            }
         }
 
         x = -3.4065f;
@@ -29,8 +45,20 @@ public class Synthesizer : MonoBehaviour
         {
             newKey = Instantiate(black_key_prefab, synthesizer.transform);
             newKey.localPosition = new Vector3(x, 0.27f, 0.05f);
+            SynthSound thisScript = newKey.gameObject.GetComponent<SynthSound>();
+            thisScript.base_frequency = black_key_freq[blackIndex];
+            thisScript.calcOctaveFreq((j / 4) + 2);
+            x += 0.327f;
+            if (blackIndex == 4)
+            {
+                blackIndex = 0;
+            }
+            else
+            {
+                blackIndex++;
+            }
 
-            if(counter < numKeys)
+            if (counter < numKeys)
             {
                 x += 0.327f;
                 counter++;
